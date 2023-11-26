0f50bdc04cc0b35d067e0ee2ace8a1e4c6a2c940
Kefas Gisung <kefasg71@gmail.com>
2023-11-27 00:15:45 +0100

    Installed serve-favicon

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const express = require("express");
-const cors = require("cors")
-const mongoose = require("mongoose");
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
+const path = require('path');
+require('dotenv').config();
 
-const userRoute = require('./route/userDataRoute')
+const userRoute = require('./route/userDataRoute');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors())
+// Serve favicon
+try {
+  app.use(favicon(path.join(__dirname, 'favicon.ico')));
+} catch (error) {
+  console.error('Error setting favicon:', error);
+}
 
-app.use('/api', userRoute)
+app.use(express.json());
+app.use(cors());
+app.use('/api', userRoute);
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("connected to database"))
-  .catch((err) => console.log("Error", err));
+  .then(() => console.log('Connected to the database'))
+  .catch((err) => console.error('Error connecting to the database:', err));
 
-
-
-const port = process.env.PORT || 3001
-app.listen(port, () => console.log("connected on port 3001"));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
